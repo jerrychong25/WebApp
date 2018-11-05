98caebbe1a481c8951e278cf598bea7c26c5f473
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-11-05 23:47:40 +0800

    Added FriendlyChatFunctions

---
+++ FriendlyChatFunctions/functions/index.js
@@ -0,0 +1,25 @@
+const functions = require('firebase-functions');
+
+// replaces keywords with emoji in the "text" key of messages
+// pushed to /messages
+exports.emojify =
+    functions.database.ref('/messages/{pushId}/text')
+    .onWrite((change,context) => {
+        console.log("emojifying!");
+        // Get the value from the 'text' key of the message
+        const originalText = change.after.val();
+        const emojifiedText = emojifyText(originalText);
+
+        // Return a JavaScript Promise to update the database node
+        return change.after.ref.set(emojifiedText);
+    });
+
+// Returns text with keywords replaced by emoji
+// Replacing with the regular expression /.../ig does a case-insensitive
+// search (i flag) for all occurrences (g flag) in the string
+function emojifyText(text) {
+    var emojifiedText = text;
+    emojifiedText = emojifiedText.replace(/\blol\b/ig, "😂");
+    emojifiedText = emojifiedText.replace(/\bcat\b/ig, "😸");
+    return emojifiedText;
+}
