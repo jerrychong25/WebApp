68579fa42c79dcb51104a18e99a8d8083658b4ba
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-12-29 19:43:21 +0800

    Added POST flow under TestHandler

--- Back End/AppEngineHelloWorld/main.py
+++ Back End/AppEngineHelloWorld/main.py
@@ -15,7 +15,7 @@
 import webapp2
 
 form="""
-<form action="http://www.google.com/search">
+<form method="post" action="/testform">
     <input name="q">
     <input type="submit">
 </form>
@@ -23,9 +23,18 @@ form="""
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        #self.response.headers['Content-Type'] = 'text/plain'
+        # self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(form)
+
+class TestHandler(webapp2.RequestHandler):
+    def post(self):
+        q = self.request.get("q")
+        self.response.write(q)
+        # Debug HTTP Request
+        # self.response.headers['Content-Type'] = 'text/plain'
+        # self.response.write(self.request)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/testform', TestHandler),
 ], debug=True)
