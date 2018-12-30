db8e20fdbdd3849af3af5b10364f8edb5f9efacc
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-12-30 18:21:25 +0800

    Removed TestHandler, and modified UI

--- Back End/AppEngineHelloWorld/main.py
+++ Back End/AppEngineHelloWorld/main.py
@@ -15,26 +15,31 @@
 import webapp2
 
 form="""
-<form method="post" action="/testform">
-    <input name="q">
+<form method="post">
+    What is your birthday?
+    <br>
+    <label>
+        Month
+        <input type="text" name="month">
+    </label>
+    <label>
+        Day
+        <input type="text" name="day">
+    </label>
+    <label>
+        Year
+        <input type="text" name="year">
+    </label>
+    <br>
+    <br>
     <input type="submit">
 </form>
 """
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        # self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(form)
-
-class TestHandler(webapp2.RequestHandler):
-    def post(self):
-        q = self.request.get("q")
-        self.response.write(q)
-        # Debug HTTP Request
-        # self.response.headers['Content-Type'] = 'text/plain'
-        # self.response.write(self.request)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/testform', TestHandler),
 ], debug=True)
