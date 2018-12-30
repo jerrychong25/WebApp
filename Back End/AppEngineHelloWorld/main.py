4e814cf7f31a2c0b9c2dfadb6b397b65dbe68c5d
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-12-30 20:30:04 +0800

    Added error message when invalid data is entered

--- Back End/AppEngineHelloWorld/main.py
+++ Back End/AppEngineHelloWorld/main.py
@@ -30,6 +30,7 @@ form="""
         Year
         <input type="text" name="year">
     </label>
+    <div style="color: red">%(error)s</div>
     <br>
     <br>
     <input type="submit">
@@ -69,15 +70,17 @@ def valid_year(year):
             return year
 
 class MainPage(webapp2.RequestHandler):
+    def write_form(self, error=""):
+        self.response.write(form % {"error": error})
     def get(self):
-        self.response.write(form)
+        self.write_form()
     def post(self):
         user_month = valid_month(self.request.get('month'))
         user_day = valid_day(self.request.get('day'))
         user_year = valid_year(self.request.get('year'))
         
         if not (user_month and user_day and user_year):
-            self.response.write(form)
+            self.write_form("That doesn't look valid to me, friend.")
         else:
             self.response.write("Thanks! That's a totally valid day!")
 
