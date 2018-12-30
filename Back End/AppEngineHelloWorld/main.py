af447ee0489fc4a0778d2afc18064b6b2ededac1
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-12-30 19:59:28 +0800

    Added user input validation for day, month and year

--- Back End/AppEngineHelloWorld/main.py
+++ Back End/AppEngineHelloWorld/main.py
@@ -36,11 +36,50 @@ form="""
 </form>
 """
 
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+
+month_abbvs = dict((m[:3].lower(),m) for m in months)
+
+def valid_month(month):
+    if month:
+        short_month = month[:3].lower()
+        return month_abbvs.get(short_month)
+
+def valid_day(day):
+    if day and day.isdigit():
+        day = int(day)
+        if day > 0 & day <= 31:
+            return day
+
+def valid_year(year):
+    if year and year.isdigit():
+        year = int(year)
+        if year >= 1900 and year <=2020:
+            return year
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.write(form)
     def post(self):
-        self.response.write("Thanks! That's a totally valid day!")
+        user_month = valid_month(self.request.get('month'))
+        user_day = valid_day(self.request.get('day'))
+        user_year = valid_year(self.request.get('year'))
+        
+        if not (user_month and user_day and user_year):
+            self.response.write(form)
+        else:
+            self.response.write("Thanks! That's a totally valid day!")
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
