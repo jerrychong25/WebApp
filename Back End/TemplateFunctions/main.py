211fe2ff976101305ee87617280c12fedb8f8bc6
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-12-31 22:42:54 +0800

    Added displaying of food under Shopping List, and removed unused codes

--- Back End/TemplateFunctions/main.py
+++ Back End/TemplateFunctions/main.py
@@ -20,30 +20,6 @@ import webapp2
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
-form_html="""
-<form>
-<h2>Add a Food</h2>
-<input type="text" name="food">
-%s
-<button>Add</button>
-</form>
-"""
-
-item_html = "<li>%s</li>"
-
-hidden_html="""
-<input type="hidden" name="food" value="%s">
-"""
-
-shopping_list_html="""
-<br>
-<br>
-<h2>Shopping List</h2>
-<ul>
-%s
-</ul>
-"""
-
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.write(*a, **kw)
@@ -57,29 +33,10 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
     def get(self):
-        n = self.request.get("n")
-        if n:
-            n = int(n)
-        self.render("shopping_list.html", n=n)
+        items = self.request.get_all("food")
+        self.render("shopping_list.html", items = items)
 
-        # output = form_html
-        # output_hidden = ""
-
-        # items = self.request.get_all("food")
-        # if items:
-        #     output_items = ""
-        #     for item in items:
-        #         output_hidden += hidden_html % item
-        #         output_items += item_html % item
-
-        #     output_shopping = shopping_list_html % output_items
-        #     output += output_shopping
-
-        # output = output % output_hidden
-
-        # self.write(output)
-
-class FizzBuzzHandler(handler):
+class FizzBuzzHandler(Handler):
     def get(self):
         n = self.request.get('n', 0)
         n = n and int(n)
