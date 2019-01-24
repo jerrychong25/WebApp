45be2700bf8942d07743c0d416fdd3fdbb398c7d
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2019-01-24 17:21:21 +0800

    Added Cookie hashing with MD5 algorithm to prevent user cheating

--- Back End/CookieFunctions/main.py
+++ Back End/CookieFunctions/main.py
@@ -22,6 +22,19 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), 
                                 autoescape = True)
 
+import hashlib
+
+def hash_str(s):
+    return hashlib.md5(s).hexdigest()
+
+def make_secure_val(s):
+    return "%s|%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+    val = h.split('|')[0]
+    if h == make_secure_val(val):
+        return val
+
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.write(*a, **kw)
@@ -36,14 +49,18 @@ class Handler(webapp2.RequestHandler):
 class MainPage(Handler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        visits = self.request.cookies.get('visits','0')
-        #make sure visits is an int
-        if visits.isdigit():
-            visits = int(visits) + 1
-        else:
-            visits = 0
+        visits = 0
+        visit_cookie_str = self.request.cookies.get('visits')
+        if visit_cookie_str:
+            cookie_val = check_secure_val(visit_cookie_str)
+            if cookie_val:
+                visits = int(cookie_val)
 
-        self.response.headers.add_header('Set-Cookie', 'visits=%s' % visits)
+        visits += 1
+
+        new_cookie_val = make_secure_val(str(visits))
+
+        self.response.headers.add_header('Set-Cookie', 'visits=%s' % new_cookie_val)
 
         if visits > 10:
             self.write("You are the best ever!")
